48a5e9c688e803a553ea0ec98511f499683e0705
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-03-20 19:04:18 +0100

    Add resources definition

---
+++ resources.h
@@ -0,0 +1,12 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+#include <windows.h>
+
+#define IDR_RCDATA1                     300
+
+#define IDS_STRING1                     1
+#define IDS_STRING2                     2
+#define IDS_STRING3                     3
+
+#endif
